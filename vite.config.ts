42589b2e48531f3a247bda345ef593df9aeccdeb
Grombolar <31845823+Grombolar@users.noreply.github.com>
2025-06-27 10:02:05 +0800

    feat: 添加类型
    添加引入时的类型

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
 import { defineConfig } from 'vite'
 import path from 'path';
 import yaml from '@rollup/plugin-yaml';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [yaml()],
+  plugins: [
+    yaml(),
+    dts({
+      entryRoot: 'src',
+      outDir: 'dist/types',
+      tsconfigPath: './tsconfig.json'
+    })
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'time-zones',
-      fileName: (format) => `time-zones.${format}.js`,
+      name: 'win-timezone-utils',
+      fileName: (format) => `win-timezone-utils.${format}.js`,
       formats: ['es', 'cjs']
     },
+    rollupOptions: {
+      external: []
+    }
   }
 })
